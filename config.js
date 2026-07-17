2f8278798f46671b9a7b77e3d5b7d7d0bad5cdef
rokodinga <rokodinga@gmail.com>
2026-07-17 11:53:51 +0530

    Add debug token to configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.DEFAULT_CONFIG = {
-  // Paste your Apps Script Web App URL below (must end in /exec)
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbyF5Z7JSn2IZ_Wv7v8j13Bja7DFpwHHWr916UIJvg7v2YTmq5CZt_8zkOpfKldPfI0z/exec', 
-  
-  // Leave this blank. Users will enter the key on the login screen.
-  apiKey: '' 
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbyF5Z7JSn2IZ_Wv7v8j13Bja7DFpwHHWr916UIJvg7v2YTmq5CZt_8zkOpfKldPfI0z/exec',
+  apiKey: ''
 };
+
+// Debug helper
+window.DEBUG_TOKEN = '4db83321-75e4-4a79-b2dd-ea04d83cbc9e';
